6aca3aebd75eb8d9a9e26c0866ca2064cd599311
Christophe Hamerling <chamerling@linagora.com>
2015-10-24 00:44:04 +0200

    Let's drink beers!

---
+++ bin/beerbot.js
@@ -0,0 +1,20 @@
+'us strict';
+
+var bot = require('../lib/index');
+var token = process.env.SLACK_TOKEN;
+
+var options = {
+  token: token,
+  silent: false,
+  listen_on: ['jenkins'],
+  reply_on: 'general',
+  expression: /Failure after/,
+  term: 'beer'
+};
+
+bot(options).then(function(slack) {
+  console.log('Started');
+  // TODO disconnect
+}, function(err) {
+  console.error('Error while starting bot', err);
+});
