c519a91281650684ea00d3327c7387fd766c80d3
Christophe Hamerling <chamerling@linagora.com>
2015-10-28 23:12:31 +0100

    update to listen to jenkins channel

--- bin/beerbot.js
+++ bin/beerbot.js
@@ -1,4 +1,4 @@
-'us strict';
+'use strict';
 
 var bot = require('../lib/index');
 var token = process.env.SLACK_TOKEN;
@@ -6,13 +6,13 @@ var token = process.env.SLACK_TOKEN;
 var options = {
   token: token,
   silent: false,
-  listen_on: ['jenkins'],
+  listen_on: ['#jenkins'],
   reply_on: 'general',
   expression: /Failure after/,
   term: 'beer'
 };
 
-bot(options).then(function(slack) {
+bot(options).then(function() {
   console.log('Started');
   // TODO disconnect
 }, function(err) {
