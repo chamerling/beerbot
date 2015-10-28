5786ec7e664e003b106ba8841938815748f2d9e5
Christophe Hamerling <chamerling@linagora.com>
2015-10-28 23:12:50 +0100

    lint them all!

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,19 @@ var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
-gulp.task('static', function () {
+gulp.task('static', function() {
   return gulp.src('**/*.js')
     .pipe(excludeGitignore())
-    //.pipe(eslint())
-    //.pipe(eslint.format())
-    //.pipe(eslint.failAfterError());
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('nsp', function (cb) {
+gulp.task('nsp', function(cb) {
   nsp('package.json', cb);
 });
 
-gulp.task('pre-test', function () {
+gulp.task('pre-test', function() {
   return gulp.src('lib/**/*.js')
     .pipe(istanbul({
       includeUntested: true
@@ -28,17 +28,17 @@ gulp.task('pre-test', function () {
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function (cb) {
+gulp.task('test', ['pre-test'], function(cb) {
   var mochaErr;
 
   gulp.src('test/**/*.js')
     .pipe(plumber())
     .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
+    .on('error', function(err) {
       mochaErr = err;
     })
     .pipe(istanbul.writeReports())
-    .on('end', function () {
+    .on('end', function() {
       cb(mochaErr);
     });
 });
--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 var assert = require('assert');
-var beerbot = require('../lib');
 
-describe('beerbot', function () {
-  it('should have unit test!', function () {
+describe('beerbot', function() {
+  it('should have unit test!', function() {
     assert(true, 'we expected this package author to add actual unit tests.');
   });
 });
